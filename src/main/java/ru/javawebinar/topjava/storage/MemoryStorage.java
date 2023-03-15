387632f5163ae0e81fd7ec849d325be2fc939ad1
Nikita <nikitamasslennikov@mail.ru>
2023-03-15 15:14:07 +0300

    HW1 with synchronizedMap

--- src/main/java/ru/javawebinar/topjava/storage/MemoryStorage.java
+++ src/main/java/ru/javawebinar/topjava/storage/MemoryStorage.java
@@ -7,48 +7,41 @@ import java.time.Month;
 import java.util.*;
 
 public class MemoryStorage implements MealsStorage {
-    private List<Meal> storage = new ArrayList<>();
+    private Map<String, Meal> storage = Collections.synchronizedMap(new LinkedHashMap<>());
 
     public MemoryStorage() {
-        storage.addAll(Arrays.asList(
-                new Meal(LocalDateTime.of(2020, Month.JANUARY, 30, 10, 0), "Завтрак", 500),
-                new Meal(LocalDateTime.of(2020, Month.JANUARY, 30, 13, 0), "Обед", 1000),
-                new Meal(LocalDateTime.of(2020, Month.JANUARY, 30, 20, 0), "Ужин", 500),
-                new Meal(LocalDateTime.of(2020, Month.JANUARY, 31, 0, 0), "Еда на граничное значение", 100),
-                new Meal(LocalDateTime.of(2020, Month.JANUARY, 31, 10, 0), "Завтрак", 1000),
-                new Meal(LocalDateTime.of(2020, Month.JANUARY, 31, 13, 0), "Обед", 500),
-                new Meal(LocalDateTime.of(2020, Month.JANUARY, 31, 20, 0), "Ужин", 410)
-        ));
-    }
-
-    private Integer getIndex(String uuid) {
-        for (int i = 0; i < storage.size(); i++) {
-            if (storage.get(i).getUuid().equals(uuid)) {
-                return i;
-            }
-        }
-        return null;
+//        storage.addAll(Arrays.asList(
+//                new Meal(LocalDateTime.of(2020, Month.JANUARY, 30, 10, 0), "Завтрак", 500),
+//                new Meal(LocalDateTime.of(2020, Month.JANUARY, 30, 13, 0), "Обед", 1000),
+//                new Meal(LocalDateTime.of(2020, Month.JANUARY, 30, 20, 0), "Ужин", 500),
+//                new Meal(LocalDateTime.of(2020, Month.JANUARY, 31, 0, 0), "Еда на граничное значение", 100),
+//                new Meal(LocalDateTime.of(2020, Month.JANUARY, 31, 10, 0), "Завтрак", 1000),
+//                new Meal(LocalDateTime.of(2020, Month.JANUARY, 31, 13, 0), "Обед", 500),
+//                new Meal(LocalDateTime.of(2020, Month.JANUARY, 31, 20, 0), "Ужин", 410)
+//        ));
+        Meal meal = new Meal(LocalDateTime.of(2020, Month.JANUARY, 30, 10, 0), "Завтрак", 500);
+        storage.put(meal.getUuid(), meal);
     }
 
     public Meal create(Meal meal) {
-        storage.add(meal);
+        storage.put(meal.getUuid(), meal);
         return meal;
     }
 
     public void delete(String uuid) {
-        storage.remove(Objects.requireNonNull(getIndex(uuid)).intValue());
+        storage.remove(uuid);
     }
 
     public Meal update(Meal meal) {
-        storage.set(Objects.requireNonNull(getIndex(meal.getUuid())), meal);
+        storage.put(meal.getUuid(), meal);
         return meal;
     }
 
     public Meal get(String uuid) {
-        return storage.get(Objects.requireNonNull(getIndex(uuid)).intValue());
+        return storage.get(uuid);
     }
 
     public List<Meal> getAll() {
-        return storage;
+        return new ArrayList<>(storage.values());
     }
 }
