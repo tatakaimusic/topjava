001ee0b5c80b77d3e9e07b1e0d40d71b6c18927e
Nikita <nikitamasslennikov@mail.ru>
2023-03-21 20:31:21 +0300

    HW2 part 4

--- src/main/java/ru/javawebinar/topjava/web/meal/MealRestController.java
+++ src/main/java/ru/javawebinar/topjava/web/meal/MealRestController.java
@@ -1,8 +1,50 @@
 package ru.javawebinar.topjava.web.meal;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.service.MealService;
 
-public class MealRestController {
-    private MealService service;
+import java.util.List;
 
+import static ru.javawebinar.topjava.util.ValidationUtil.assureIdConsistent;
+
+@Controller
+public class MealRestController {
+    protected final Logger log = LoggerFactory.getLogger(getClass());
+
+    private final MealService service;
+
+    @Autowired
+    public MealRestController(MealService service) {
+        this.service = service;
+    }
+
+    public Meal create(Meal meal, int userId) {
+        log.info("create {} with userId={}", meal, userId);
+        return service.create(meal, userId);
+    }
+
+    public void update(Meal meal, int id, int userId) {
+        log.info("update {} with id={} where userId={}", meal, id, userId);
+        assureIdConsistent(meal, id);
+        service.update(meal, userId);
+    }
+
+    public void delete(int id, int userId) {
+        log.info("delete {} where userId={}", id, userId);
+        service.delete(id, userId);
+    }
+
+    public Meal get(int id, int userId) {
+        log.info("get {} where userId={}", id, userId);
+        return service.get(id, userId);
+    }
+
+    public List<Meal> getAll(int userId) {
+        log.info("get all where userId={}", userId);
+        return service.getAll(userId);
+    }
 }
